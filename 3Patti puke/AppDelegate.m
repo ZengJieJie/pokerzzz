8121e0059ced2d0199b91b9390ce2b6cc298783c
adin <adina@192.168.2.4>
2024-04-04 18:29:51 +0800

    adfasfdas

---
+++ 3Patti puke/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  3Patti puke
+//
+//  Created by adin on 2024/3/25.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+
+
+
+@end
